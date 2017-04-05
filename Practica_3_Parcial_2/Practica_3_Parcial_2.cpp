7de2d74ba1f0a4a6e7bcdaa0cc6ee397a2636701
Efraín Girón <efrainbond@hotmail.com>
2017-04-05 07:39:55 -0500

    Función principal terminada

--- Practica_3_Parcial_2/Practica_3_Parcial_2.cpp
+++ Practica_3_Parcial_2/Practica_3_Parcial_2.cpp
@@ -1,11 +1,85 @@
-// Practica_3_Parcial_2.cpp: define el punto de entrada de la aplicación de consola.
-//
-
+//Efraín Girón Romero 15300102
 #include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
 
+using namespace std;
 
-int main()
-{
-    return 0;
+//Función para pocicionarse en un lugar especifico de la pantalla.
+void gotoxy(int x, int y) {
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
 }
 
+class Elemento {
+private:
+	//Coordenadas
+	int x, y;
+public:
+	//Función para asignar el valor inicial de x
+	void setX(int a) {
+		x = a;
+	}
+	//Función para asignar el valor inicial de y
+	void setY(int b) {
+		y = b;
+	}
+	//Función para mover el objeto
+	void mover() {
+		if (_kbhit()) {
+			int tecla = _getch();
+			//Condicionales para mover el objeto con las teclas
+			switch (tecla)
+			{
+				//Derecha
+			case 77:
+				x++;
+				break;
+				//Izquierda
+			case 75:
+				x--;
+				break;
+				//Abajo
+			case 80:
+				y++;
+				break;
+				//Arriba
+			case 72:
+				y--;
+				break;
+			}
+		}
+		//Condicionales para definir el borde
+		if (x > 78) {
+			x--;
+		}
+		if (x < 0) {
+			x++;
+		}
+		if (y > 25) {
+			y--;
+		}
+		if (y < 0) {
+			y++;
+		}
+		gotoxy(x, y);
+		cout << ":)" << endl;
+	}
+};
+
+int main() {
+	Elemento Caracter;
+	Caracter.setX(40);
+	Caracter.setY(12);
+	bool repetir = true;
+	while (repetir) {
+		Caracter.mover();
+		system("cls");
+	}
+	return 0;
+}
