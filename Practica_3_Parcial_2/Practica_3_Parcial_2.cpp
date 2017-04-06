747206d75ef1710b43d3a33cec7a13d571878994
Efraín Girón <efrainbond@hotmail.com>
2017-04-05 20:21:04 -0500

    Agregados comentarios

--- Practica_3_Parcial_2/Practica_3_Parcial_2.cpp
+++ Practica_3_Parcial_2/Practica_3_Parcial_2.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 //Función para pocicionarse en un lugar especifico de la pantalla.
+//Function to move us somewhere in the console
 void gotoxy(int x, int y) {
 	HANDLE hcon;
 	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -19,42 +20,52 @@ void gotoxy(int x, int y) {
 class Elemento {
 private:
 	//Coordenadas
+	//Coodinates
 	int x, y;
 public:
 	//Función para asignar el valor inicial de x
+	//Function to assign the value to the x coordinate
 	void setX(int a) {
 		x = a;
 	}
 	//Función para asignar el valor inicial de y
+	//Function to assign the value to the y coordinate
 	void setY(int b) {
 		y = b;
 	}
 	//Función para mover el objeto
+	//Function to move the element
 	void mover() {
 		if (_kbhit()) {
 			int tecla = _getch();
 			//Condicionales para mover el objeto con las teclas
+			//Conditions to move the object with the keyboard
 			switch (tecla)
 			{
 				//Derecha
+				//Right
 			case 77:
 				x++;
 				break;
 				//Izquierda
+				//Left
 			case 75:
 				x--;
 				break;
 				//Abajo
+				//Down
 			case 80:
 				y++;
 				break;
 				//Arriba
+				//Up
 			case 72:
 				y--;
 				break;
 			}
 		}
 		//Condicionales para definir el borde
+		//Conditions to define the limit
 		if (x > 78) {
 			x--;
 		}
@@ -70,6 +81,8 @@ public:
 		gotoxy(x, y);
 		cout << ":)";
 	}
+	//Función para borrar el elemento de la pantalla
+	//Function to delete the element of the display
 	void borrar() {
 		gotoxy(x, y);
 		cout << "  ";
